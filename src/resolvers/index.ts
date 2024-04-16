9646dc2c8a93d6620c98525ff3eb64ddef37f095
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-16 13:45:49 +0200

    FIX: resolver params

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -19,16 +19,16 @@ import {transformGitHubData} from "@/utils/index";
 
 export const resolvers: Resolvers = {
     Query: {
-        basics: async (context: AppContext) => await getBasics(context),
-        work: async (context: AppContext) => await getWork(context),
-        volunteer: async (context: AppContext) => await getVolunteer(context),
-        education: async (context: AppContext) => await getEducation(context),
-        awards: async (context: AppContext) => await getAwards(context),
-        publications: async (context: AppContext) => await getPublications(context),
-        languages: async (context: AppContext) => await getLanguages(context),
-        gitHubRepos: async (context: AppContext) => await getGitHubRepos(context),
-        getReadingStates: async (context: AppContext) => await getMyReadingStates(context),
-        getLiteralToken: async (context: AppContext) => await getToken(context),
+        basics: async (parent: any, args: any, context: AppContext) => await getBasics(context),
+        work: async (parent: any, args: any, context: AppContext) => await getWork(context),
+        volunteer: async (parent: any, args: any, context: AppContext) => await getVolunteer(context),
+        education: async (parent: any, args: any, context: AppContext) => await getEducation(context),
+        awards: async (parent: any, args: any, context: AppContext) => await getAwards(context),
+        publications: async (parent: any, args: any, context: AppContext) => await getPublications(context),
+        languages: async (parent: any, args: any, context: AppContext) => await getLanguages(context),
+        gitHubRepos: async (parent: any, args: any, context: AppContext) => await getGitHubRepos(context),
+        getReadingStates: async (parent: any, args: any, context: AppContext) => await getMyReadingStates(context),
+        getLiteralToken: async (parent: any, args: any, context: AppContext) => await getToken(context),
     },
     Mutation: {
         login: (parent: any, args: { email: string, password: string }) => UserLogin.login,
@@ -36,21 +36,21 @@ export const resolvers: Resolvers = {
 };
 export const devResolvers: Resolvers = {
     Query: {
-        basics: async (context: AppContext) => RESUME.basics,
-        work: async (context: AppContext) => RESUME.work,
-        volunteer: async (context: AppContext) => RESUME.volunteer,
-        education: async (context: AppContext) => RESUME.education,
-        awards: async (context: AppContext) => RESUME.awards,
-        publications: async (context: AppContext) => RESUME.publications,
-        languages: async (context: AppContext) => RESUME.languages,
-        gitHubRepos: async (context: AppContext) => {
+        basics: async (parent: any, args: any, context: AppContext) => RESUME.basics,
+        work: async (parent: any, args: any, context: AppContext) => RESUME.work,
+        volunteer: async (parent: any, args: any, context: AppContext) => RESUME.volunteer,
+        education: async (parent: any, args: any, context: AppContext) => RESUME.education,
+        awards: async (parent: any, args: any, context: AppContext) => RESUME.awards,
+        publications: async (parent: any, args: any, context: AppContext) => RESUME.publications,
+        languages: async (parent: any, args: any, context: AppContext) => RESUME.languages,
+        gitHubRepos: async (parent: any, args: any, context: AppContext) => {
             const urls = GITHUB_REPOS.map((repo: any) => repo.html_url);
             const rawMeta: Promise<GitHubRepo>[] = urls.map(async (url: string) => fetchMeta(url));
             const meta: GitHubRepo[] = await Promise.all(rawMeta);
             return transformGitHubData(meta, GITHUB_REPOS[0].owner)
         },
-        getReadingStates: async (context: AppContext) => ReadingStates.myReadingStates,
-        getLiteralToken: async (context: AppContext) => DEFAULT_LITERAL_TOKEN,
+        getReadingStates: async (parent: any, args: any, context: AppContext) => ReadingStates.myReadingStates,
+        getLiteralToken: async (parent: any, args: any, context: AppContext) => DEFAULT_LITERAL_TOKEN,
     },
     Mutation: {
         login: (parent: any, args: { email: string, password: string }) => UserLogin.login,
